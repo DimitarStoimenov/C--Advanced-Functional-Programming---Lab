b47e879c7585f081171b14a5f6107b71b403c46c
DimitarStoimenov <stoimenovdimitur@yahoo.com>
2022-06-04 23:59:19 +0200

    C#

---
+++ Sort Even Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(string.Join(", ", Console.ReadLine().Split(", ").Select(int.Parse).Where(x => x % 2 == 0).OrderBy(x => x).ToArray()));
+
+        }
+
+    }
+}
+
---
+++ SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] nums = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            Console.WriteLine(nums.Count());
+            Console.WriteLine(nums.Sum());
+        }
+
+    }
+}
+
